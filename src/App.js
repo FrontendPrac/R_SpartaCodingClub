2d245e16a8ac669d85acc9641297d0e05045eb85
Kidongg <dlrlehd7@daum.net>
2023-05-04 01:39:56 +0900

    context API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import TextBox from "./TextBox";
-import Input from "./Input";
-import { useInput } from "./useInput";
 import { createContext, useContext, useState } from "react";
 
 function App() {
-  // custom Hook
-  const [text, setText, inputRef] = useInput("");
-
   // context API
   const [name, setName] = useState("kidongg");
   const MyStore = createContext();
@@ -28,11 +22,6 @@ function App() {
         <MyStoreConsumer />
       </MyStore.Provider>
     </>
-    // custom Hook
-    // <div className="App" style={{ display: "flex", gap: "10px" }}>
-    //   <TextBox text={text} />
-    //   <Input setText={setText} inputRef={inputRef} />
-    // </div>
   );
 }
 
